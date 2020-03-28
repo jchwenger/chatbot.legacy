326ae7e8732ee3a758480c963267518602d7ca3f
jchwenger <34098722+jchwenger@users.noreply.github.com>
2020-03-28 15:17:54 +0000

    app: overhaul of printing --> using logging module

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ import tensorflow as tf
 import encoder
 import uvicorn
 import asyncio
+import logging
 import pprint
 import sample
 import model
@@ -20,13 +21,6 @@ import gc
 
 pp = pprint.PrettyPrinter(indent=2)
 
-
-def cprint(x):
-    print()
-    print(x)
-    print("-" * len(x))
-
-
 # disabling some warnings
 os.environ["KMP_WARNINGS"] = "off"
 
@@ -36,6 +30,41 @@ app = Starlette(debug=False, middleware=middleware)
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 templates = Jinja2Templates(directory="templates")
+
+# https://github.com/encode/uvicorn/issues/523#issuecomment-598522664
+logger = logging.getLogger("uvicorn")
+logging.getLogger('uvicorn.access').propagate = True
+logger.addHandler(logging.FileHandler("logs/errors.log"))
+logger.setLevel(logging.DEBUG)
+
+# log utils
+def custom_log(x, level="INFO", offset=""):
+    if level == "INFO":
+        logger.info(f"{offset}{x}")
+    if level == "WARNING":
+        logger.warning(f"{offset}{x}")
+    if level == "ERROR":
+        logger.error(f"{offset}{x}")
+
+def overlog(x, level="INFO", offset=""):
+    custom_log("-" * len(x), level=level, offset=offset)
+    custom_log(x, level=level, offset=offset)
+    custom_log(" ", level=level, offset=offset)
+
+def underlog(x, level="INFO", offset=""):
+    custom_log(" ", level=level, offset=offset)
+    custom_log(x, level=level, offset=offset)
+    custom_log("-" * len(x), level=level, offset=offset)
+
+def sandwich_log(x, level="INFO", offset=""):
+    custom_log("-" * len(x), level=level, offset=offset)
+    custom_log(x, level=level, offset=offset)
+    custom_log("-" * len(x), level=level, offset=offset)
+
+custom_log(" ")
+custom_log("="*40)
+custom_log(" " * 17 + "NEW RUN")
+custom_log("="*40)
 
 
 config = tf.compat.v1.ConfigProto()
@@ -61,10 +90,7 @@ ckpt = tf.train.latest_checkpoint("checkpoint/run1")
 saver = tf.compat.v1.train.Saver(allow_empty=True)
 sess.run(tf.compat.v1.global_variables_initializer())
 
-msg = f"Loading checkpoint {ckpt}"
-print("-" * len(msg))
-print(msg)
-print("-" * len(msg))
+sandwich_log(f"Loading checkpoint {ckpt}")
 saver.restore(sess, ckpt)
 
 enc = encoder.get_encoder("run1")
@@ -86,8 +112,9 @@ output = sample.sample_sequence(
 
 out = sess.run(output, feed_dict={length: 1, context: [context_tokens]})
 
-print("-" * 40)
-cprint(f"dummy run preformed: {enc.decode(out[0])}")
+# # for belly-of-the-beast-decoding, see encoder.py
+# sandwich_log(f"Dummy run preformed: {enc.decode(out[0])[0]}")
+sandwich_log(f"Dummy run preformed: {enc.decode(out[0])}")
 
 # find first response in gpt stream
 pref_re = regex.compile("(?<=<\|s\|>\n).*?(?=\n<\|e\|>)", regex.DOTALL)
@@ -121,8 +148,8 @@ def generate(params):
     global pp
     global r
 
-    # cprint("previous prefix:")
-    # print(new_pref)
+    # underlog("previous prefix:")
+    # custom_log(new_pref)
 
     char_name = params.get("character", "")
     input_orig = params.get("prefix", "").strip()
@@ -130,20 +157,26 @@ def generate(params):
 
     # first request, we add the char name & user input
     if char_name and input_orig:
-        # print("adding char and user input")
+        # custom_log("adding char and user input")
+        # underlog("user input:")
+        # custom_log(char_name)
+        # custom_log(input_orig)
         pref = f"{new_pref}{start}{char_name}\n{input_orig}{end}"
     else:
         pref = new_pref
 
-    cprint("prefix:")
-    print(pref)
+    # underlog("prefix:")
+    # custom_log(pref)
 
     # add end of answer, store length of prefix
     end_pref = len(pref)
 
     context_tokens = enc.encode(pref)
     l = len(context_tokens)
-    # cprint(f"current length {l}")
+    # underlog(f"current length {l}")
+
+    # underlog('context tokens:', offset='\t\t\t\t')
+    # custom_log(context_tokens, offset='\t\t\t\t')
 
     # checks for length, in case input is very long
     # max_length = 1023 - length_desired
@@ -152,7 +185,8 @@ def generate(params):
         context_tokens = context_tokens[-max_length:]
         l = len(context_tokens)
         end_pref = len(enc.decode(context_tokens))
-        cprint(f"exceeding 1023 total tokens in regenerating, trimmed length: {l}")
+        # underlog(f"exceeding {limit} total tokens in regenerating", level='WARNING')
+        # custom_log(f"end of prefix at index {end_pref}, trimmed context length: {l}", level='WARNING')
 
     out = sess.run(
         output, feed_dict={context: 1 * [context_tokens], length: length_desired},
@@ -160,28 +194,30 @@ def generate(params):
 
     pref = enc.decode(out[0])
 
-    # cprint("current pref:")
-    # print(pref)
+
+    # underlog("current pref:")
+    # custom_log(pref)
 
     l_no_pref = pref[end_pref:]
     new_length = len(l_no_pref)
 
-    cprint("produced:")
-    print(l_no_pref)
-
-    # produced += new_length - produced
+    l_enc = l_no_pref.encode('utf-8')
+    custom_log(f"{l_no_pref}")
+    custom_log(f"| utf-8: {l_enc} | tokens: {out[0,-length_desired:]}", offset="\t\t\t\t")
 
     m = regex.search(r, l_no_pref)
     if m:
-        cprint("found end marker")
-        # print(l_no_pref)
+        underlog("found end marker")
+        # custom_log(l_no_pref)
         end_ind = m.span()[0]
         new_pref = f"{pref[:end_pref+end_ind]}\n<|e|>"
     else:
         new_pref = pref
 
-    # cprint("new prefix stored:")
-    # print(new_pref)
+    # underlog("new prefix stored:")
+    # custom_log(new_pref)
+
+    # custom_log('-'*40)
 
     return l_no_pref
 
@@ -197,6 +233,8 @@ async def homepage(request):
         return templates.TemplateResponse("home.html", {"request": request})
     elif request.method == "POST":
         params = await request.json()
+        # underlog('received params', offset='\t\t\t\t')
+        # custom_log(params, offset='\t\t\t\t')
         answer = generate(params)
         return UJSONResponse({"text": json.dumps(answer)}, headers=response_header)
 
